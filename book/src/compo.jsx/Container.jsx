53715163395f6acff20246c565bc2856815d1cbc
niamul007 <am3994566@gmail.com>
2024-08-10 10:14:51 +0600

    add all

--- book/src/compo.jsx/Container.jsx
+++ book/src/compo.jsx/Container.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
 import Hero from './Hero'
+import ShowBook from './ShowBook'
 function Container() {
   return (
     <div>
       <Nav/>
       <Hero/>
+      <ShowBook/>
     </div>
   )
 }
--- book/src/compo.jsx/Hero.jsx
+++ book/src/compo.jsx/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <div className='hero-container'>
-      <h1>it will be done soon i should run japanese</h1>
+      <img src="hero.webp" alt="" className="hero" />
     </div>
   )
 }
